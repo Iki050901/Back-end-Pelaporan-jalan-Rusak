5c255e22d89687720e2e864738e9d003d6c0244b
justIsal <tsalmanisal@gmail.com>
2025-07-15 01:01:19 +0700

    update53

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
 const app = express();
+app.use(cors());
 app.get('/', (req, res) => res.send('Welcome to the Users API!'));
 app.all('*', (req, res) => res.send("You've tried reaching a route that doesn't exist."));
