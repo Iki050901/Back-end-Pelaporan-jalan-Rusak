b4b398bb611c93dee79dcd934f30c1723cd98c45
justIsal <tsalmanisal@gmail.com>
2025-07-14 23:53:35 +0700

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import { web } from './application/web.js';
+// import { web } from './application/web.js';
 import { logger } from './application/logging.js';
 import serverless from 'serverless-http';
+import express from 'express';
+export const web = express();
 
-
-
-// ✅ Untuk Lokal Development
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
   web.listen(PORT, () => {
@@ -12,10 +11,20 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 web.get('/', (req, res) => {
-  res.send(`
-    <h1 style="font-family: sans-serif; color: green; text-align: center; margin-top: 50px;">
-      ✅ Backend Express Berhasil Jalan di Port 3100
-    </h1>
-  `);
+  res.send(`<h1>✅ Express Backend Jalan di Vercel</h1>`);
 });
-export default serverless(web);
+
+// if (process.env.NODE_ENV !== 'production') {
+//   const PORT = process.env.PORT || 3000;
+//   web.listen(PORT, () => {
+//     logger.info(`App running at http://localhost:${PORT}`);
+//   });
+// }
+// web.get('/', (req, res) => {
+//   res.send(`
+//     <h1 style="font-family: sans-serif; color: green; text-align: center; margin-top: 50px;">
+//       ✅ Backend Express Berhasil Jalan di Port 3100
+//     </h1>
+//   `);
+// });
+// export default serverless(web);
