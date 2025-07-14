5878fa08fbc5b8f52e2127cc6edc5edebd25f2da
justIsal <tsalmanisal@gmail.com>
2025-07-15 00:18:25 +0700

    test7

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { logger } from './application/logging.js';
 import serverless from 'serverless-http';
 
 
-
+export const handler = serverless(web);
 
 // if (process.env.NODE_ENV !== 'production') {
 //   const PORT = process.env.PORT || 3000;
@@ -11,11 +11,11 @@ import serverless from 'serverless-http';
 //     logger.info(`App running at http://localhost:${PORT}`);
 //   });
 // }
-web.get('/', (req, res) => {
-  res.send(`
-    <h1 style="font-family: sans-serif; color: green; text-align: center; margin-top: 50px;">
-      ✅ Backend Express Berhasil Jalan di Port 3100
-    </h1>
-  `);
-});
-export default serverless(web);
+// web.get('/', (req, res) => {
+//   res.send(`
+//     <h1 style="font-family: sans-serif; color: green; text-align: center; margin-top: 50px;">
+//       ✅ Backend Express Berhasil Jalan di Port 3100
+//     </h1>
+//   `);
+// });
+// export default serverless(web);
