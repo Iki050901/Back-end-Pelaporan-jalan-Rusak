1bc4086bbd957433830f4467efdeb596ac928730
justIsal <tsalmanisal@gmail.com>
2025-07-15 01:04:20 +0700

    teeefe

--- index.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-const app = express();
-app.use(cors());
-app.get('/', (req, res) => res.send('Welcome to the Users API!'));
-app.all('*', (req, res) => res.send("You've tried reaching a route that doesn't exist."));
--- src/main.js
+++ src/main.js
@@ -1,25 +1,6 @@
-// import { web } from './application/web.js';
-// import { logger } from './application/logging.js';
-// import serverless from 'serverless-http';
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => res.send('Welcome to the Users API!'));
-app.all('*', (req, res) => res.send("You've tried reaching a route that doesn't exist."));
-// web.get('/', (req, res) => {
-//   res.send(`<h1>✅ Express Backend Jalan di Vercel</h1>`);
-// });
-// export default serverless(web);
+import {web} from './application/web.js';
+import {logger} from './application/logging.js';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const PORT = process.env.PORT || 3000;
-//   web.listen(PORT, () => {
-//     logger.info(`App running at http://localhost:${PORT}`);
-//   });
-// }
-// web.get('/', (req, res) => {
-//   res.send(`
-//     <h1 style="font-family: sans-serif; color: green; text-align: center; margin-top: 50px;">
-//       ✅ Backend Express Berhasil Jalan di Port 3100
-//     </h1>
-//   `);
-// });
+web.listen(process.env.PORT, () => {
+    logger.info("App Start");
+})
